a41a7ad737604f314f13fff03d244e85338f2a3f
Yusuf <yusufkinatas@gmail.com>
2018-11-28 13:20:31 +0300

    Added projects page

--- server.js
+++ server.js
@@ -44,11 +44,17 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects Page"
+  });
+});
+
 app.get("/bad", (req, res) => {
   res.send({
     errorMessage: "Unable to handle request",
   })
-})
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
